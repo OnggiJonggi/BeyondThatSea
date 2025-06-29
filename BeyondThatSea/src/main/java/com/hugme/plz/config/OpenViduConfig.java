60471643e2e0042942085e5c693d3010320e7c41
OnggiJonggi <maru403@naver.com>
2025-06-30 01:03:06 +0900

    6.30_1

--- BeyondThatSea/src/main/java/com/hugme/plz/config/OpenViduConfig.java
+++ BeyondThatSea/src/main/java/com/hugme/plz/config/OpenViduConfig.java
@@ -1,15 +1,5 @@
 package com.hugme.plz.config;
 
-import java.net.Socket;
-import java.security.cert.X509Certificate; // 인증서 타입
-
-import javax.net.ssl.HostnameVerifier;
-import javax.net.ssl.HttpsURLConnection;
-import javax.net.ssl.SSLContext; // SSLContext 생성
-import javax.net.ssl.SSLEngine;
-import javax.net.ssl.TrustManager; // TrustManager 인터페이스
-import javax.net.ssl.X509ExtendedTrustManager;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,65 +10,24 @@ import io.openvidu.java.client.OpenVidu;
 @Configuration
 @PropertySource("classpath:config/openvidu-config.properties")
 public class OpenViduConfig {
+	// 192.168.150.27
     @Value("${openvidu.url}")
     private String URL;
+    
+    // 192.168.0.11
+    @Value("${openvidu.url2}")
+    private String URL2;
     
     @Value("${openvidu.secret}")
     private String SECRET;
 
     @Bean
     public OpenVidu openViduBean() {
-        try {
-            // JVM 레벨에서 SSL 설정
-            System.setProperty("jdk.tls.client.enableSessionTicketExtension", "false");
-            System.setProperty("javax.net.ssl.trustStore", "");
-            System.setProperty("javax.net.ssl.trustStorePassword", "");
-            System.setProperty("com.sun.net.ssl.checkRevocation", "false");
-            System.setProperty("sun.security.ssl.allowUnsafeRenegotiation", "true");
-            
-            // SSL 검증 비활성화
-            disableSSLVerification();
-            
-            return new OpenVidu(URL, SECRET);
+        try {            
+            return new OpenVidu(URL2, SECRET);
         } catch (Exception e) {
             e.printStackTrace();
             throw new RuntimeException("OpenVidu 초기화 실패", e);
         }
-    }
-    
-    private void disableSSLVerification() throws Exception {
-        TrustManager[] trustAllCerts = new TrustManager[] {
-            new X509ExtendedTrustManager() {
-                @Override
-                public void checkClientTrusted(X509Certificate[] chain, String authType, Socket socket) {}
-                
-                @Override
-                public void checkServerTrusted(X509Certificate[] chain, String authType, Socket socket) {}
-                
-                @Override
-                public void checkClientTrusted(X509Certificate[] chain, String authType, SSLEngine engine) {}
-                
-                @Override
-                public void checkServerTrusted(X509Certificate[] chain, String authType, SSLEngine engine) {}
-                
-                @Override
-                public void checkClientTrusted(X509Certificate[] chain, String authType) {}
-                
-                @Override
-                public void checkServerTrusted(X509Certificate[] chain, String authType) {}
-                
-                @Override
-                public X509Certificate[] getAcceptedIssuers() { 
-                    return new X509Certificate[0]; 
-                }
-            }
-        };
-        
-        SSLContext sslContext = SSLContext.getInstance("TLS");
-        sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
-        HttpsURLConnection.setDefaultSSLSocketFactory(sslContext.getSocketFactory());
-        
-        HostnameVerifier allHostsValid = (hostname, session) -> true;
-        HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);
     }
 }
--- BeyondThatSea/src/main/resources/config/OpenVidu-ReadMe.txt
+++ BeyondThatSea/src/main/resources/config/OpenVidu-ReadMe.txt
@@ -1,45 +1,38 @@
+// 포트 4443, 3478, 40000-42000 TCP,UDP 포트 열어두기
+
 // 다운로드
-docker pull openvidu/openvidu-dev:2.31.0
 docker pull openvidu/openvidu-server-kms:2.23.0
 
-// 컨테이너 제작(dev)
-docker run --name openvidu ^
--p 4443:4443 --rm ^
--e OPENVIDU_SECRET=OnggiJonggi ^
--e DOMAIN_OR_PUBLIC_IP=192.168.150.27 ^
--e CERTIFICATE_TYPE=selfsigned ^
-openvidu/openvidu-dev:2.31.0
+// .env 파일 작성
+DOMAIN_OR_PUBLIC_IP=localhost
+OPENVIDU_SECRET=OnggiJonggi
+CERTIFICATE_TYPE=selfsigned
 
-docker run --name openvidu -p 4443:4443 --rm -e OPENVIDU_SECRET=OnggiJonggi -e DOMAIN_OR_PUBLIC_IP=192.168.150.27 -e CERTIFICATE_TYPE=selfsigned openvidu/openvidu-dev:2.31.0
+// docker-compose.yml 파일 작성
+services:
+  openvidu-server:
+    image: openvidu/openvidu-server-kms:2.23.0
+    container_name: openvidu-server
+    restart: unless-stopped
+    ports:
+      - "4443:4443"
+      - "3478:3478"
+      - "40000-42000:40000-42000"
+    environment:
+      - DOMAIN_OR_PUBLIC_IP=${DOMAIN_OR_PUBLIC_IP}
+      - OPENVIDU_SECRET=${OPENVIDU_SECRET}
+      - CERTIFICATE_TYPE=${CERTIFICATE_TYPE}
 
-docker run --name openvidu ^
-  -d ^
-  -p 4443:4443 ^
-  -e OPENVIDU_SECRET=OnggiJonggi ^
-  -e DOMAIN_OR_PUBLIC_IP=192.168.150.27 ^
-  -e CERTIFICATE_TYPE=selfsigned ^
-  openvidu/openvidu-dev:2.31.0
-
-
-// 컨테이너 제작(kms), cmd
-docker run -d --name openvidu ^
--p 4443:4443 ^
--e OPENVIDU_SECRET=OnggiJonggi ^
--e DOMAIN_OR_PUBLIC_IP=192.168.150.27 ^
--e CERTIFICATE_TYPE=owncert ^
-openvidu/openvidu-server-kms:2.23.0
-
-// 인증서 생성, cmd
-"C:\Program Files\OpenSSL-Win64\bin\openssl.exe" req ^
-  -x509 -newkey rsa:2048 ^
-  -keyout certificate.key ^
-  -out certificate.cert ^
-  -days 365 -nodes ^
-  -subj "/CN=192.168.150.27"
-  
-//인증서 복사
-docker cp certificate.key openvidu:/opt/openvidu/owncert/certificate.key
-docker cp certificate.cert openvidu:/opt/openvidu/owncert/certificate.cert
+//docker-compose 실행
+cd D:\Dev\Docker\openvidu
+docker compose up -d
 
 
+// 건방지게 docker가 유령 컨테이너를 깨작거린다면(관리자 권한)
+net stop com.docker.service
+net start com.docker.service
 
+// 더 건방지게 docker가 무한 로딩을 깨작거린다면
+wsl --unregister docker-desktop
+wsl --unregister docker-desktop-data
+// 컴퓨터 재시작
